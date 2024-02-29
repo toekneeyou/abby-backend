b066b3211cbc924ade5603919bdb51642a10305f
Johnny Siu <johnnysiu@Johnnys-MBP.attlocal.net>
2024-02-29 12:32:21 -0800

    updating app js file to run directly from that file

--- app.js
+++ app.js
@@ -38,4 +38,28 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// Get port from environment and store in Express
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// Create HTTP server
+var server = app.listen(port, function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
+// Normalize a port into a number, string, or false
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
